856072354d25455111ef14d6c3cc4b55b2807232
chae52 <fjdksla98@naver.com>
2018-07-26 17:47:11 +0900

    환경설정 2
    가운데정렬
    취소버튼
    로고추가
    비밀번호 입력칸

--- public/javascripts/settings.js
+++ public/javascripts/settings.js
@@ -1,6 +1,19 @@
 function changeMenu(x){
-    var y=document.getElementsByClassName("active item")[0];
-    y.className="item";
+   
+        var y=document.getElementsByClassName("active item")[0];
 
-    x.className="active item";
+        y.className="item";
+        x.className="active item";
+
+
+        var d= y.id+"Des"; //active item
+        var des=x.id+"Des"; //item
+        if(d!=des){
+            document.getElementById(des).style.display='block';
+            document.getElementById(d).style.display='none';
+        }
 }
+
+
+
+$('.ui.selection.dropdown').dropdown();
