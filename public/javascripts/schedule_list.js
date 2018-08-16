46fa28d5bd075e98831296d58b2d99da5ca264eb
Sain <chyh1900@gmail.com>
2018-08-16 15:01:12 +0900

    Revert "일정 추가시 데이터베이스에 저장"
    This reverts commit 28423d794b5340bee0b79df955956c02cd8e315b.

--- public/javascripts/schedule_list.js
+++ public/javascripts/schedule_list.js
@@ -9,6 +9,10 @@ $(document).ready(function(){
         enableTime: false,
         dateFormat: "Y.m.d"
     });
+    // $("#end_date").flatpickr({
+    //     enableTime: false,
+    //     dateFormat: "Y.m.d"
+    // });
     $('#time1').flatpickr({
         enableTime: true,
         noCalendar: true,
@@ -31,8 +35,9 @@ $(document).ready(function(){
     });
     
     // var startDate = flatpickr('#start_date');
-    // var startTime = flatpickr('#time1');
-    // var endTime = flatpickr('#time2');
+    var endDate = flatpickr('#end_date');
+    var startTime = flatpickr('#time1');
+    var endTime = flatpickr('#time2');
 
     // flatpickr("#start_date", {
     //     enableTime: false,
@@ -44,45 +49,70 @@ $(document).ready(function(){
     //         // endDate.set('minDate', dateStr);
     //     }
     // });
+    // flatpickr("#end_date", {
+    //     enableTime: false,
+    //     dateFormat: "Y.m.d",
+    //     onChange: function(dateObj, dateStr) {
+    //         startDate = flatpickr("#start_date", { maxDate: dateStr, dateFormat: "Y.m.d", });
+    //     }
+    // });
 
-    // $('.ui.accordion').accordion({
-    //     selector: {
-    //         trigger: '.title .icon'
-    //         }
-    //     });
+    // flatpickr('#time1', {
+    //     enableTime: true,
+    //     noCalendar: true,
+    //     dateFormat: "h:i K",
+    //     onChange: function(timeObj, timeStr) {
+    //         console.log(timeStr);
 
-    $('#repeat_dropdown').dropdown();
-    $('#repeat_details').hide();
-    $('#repeat_details2').hide();
+    //     }
+    // });
 
-    $('#p_repeat').change(function(){
-        var pRepeat=$('#p_repeat').val();
-        $('#repeating_period').html(pRepeat+'마다');
-
-        console.log(pRepeat);
-
-        if(pRepeat == 0){
-            $('#repeat_details').hide();
-            $('#repeat_details2').hide();
-        } else {
-            $('#repeat_details').show();
-            $('#repeat_details2').show();
-
-            if(pRepeat == 1) {
-                $('#repeating_period').html('일마다');
-            } else if (pRepeat == 2) {
-                $('#repeating_period').html('주마다');
-            } else if (pRepeat ==3 ) {
-                $('#repeating_period').html('개월마다');
-            } else {
-                $('#repeating_period').html('년마다');
+    $('.ui.accordion').accordion({
+        selector: {
+            trigger: '.title .icon'
             }
-        }
+        });
+
+    $('.ui.selection.dropdown').dropdown();
+
+    // 채원이거 여기부터~~!~!
+    $(".close").click(function(){
+        $('.ui.longer.modal').modal('hide');
     });
+
     $("#end_repeat").flatpickr({
         enableTime: false,
         dateFormat: "Y.m.d"
     });
+    
+    var BackgroundClass = Quill.import('attributors/class/background');
+    var ColorClass = Quill.import('attributors/class/color');
+    var SizeStyle = Quill.import('attributors/style/size');
+    Quill.register(BackgroundClass, true);
+    Quill.register(ColorClass, true);
+    Quill.register(SizeStyle, true);
+    var quill = new Quill('#editor-container', {
+        modules: {
+            toolbar:'#toolbar-container' ,
+            imageResize: {
+                displaySize: true
+              },
+            imageDrop: true
+        },
+        placeholder: '오늘의 일기를 작성해주세요',
+        theme: 'snow'  // or 'bubble'
+      });
+      
+
+    $('#취소').click(function(){
+        $('#new_diary_hnc').attr({'style':'display: block !important'});
+        $('#write_diary').attr({'style':'display: none !important'});
+    });
+
+    $('#new_diary_content').click(function(){
+        $('#new_diary_hnc').attr({'style':'display: none !important'});
+        $('#write_diary').attr({'style':'display: block !important'});
+    });
+    
 });
 
-    // 채원이거 여기부터~~!~!
